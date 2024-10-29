1929db1fc3b8dd00fd430a915be0530c6dd36202
mykytasirobaba <mykyta.sirobaba@gmail.com>
2024-10-29 21:29:19 +0200

    Eighth version

--- application/src/game/Main.java
+++ application/src/game/Main.java
@@ -8,7 +8,7 @@ public class Main {
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
         window.setTitle("2D Adventure");
-        
+
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
 
@@ -17,6 +17,7 @@ public class Main {
         window.setLocationRelativeTo(null);
         window.setVisible(true);
 
+        gamePanel.setupGame();
         gamePanel.startGameThread();
     }
 }
